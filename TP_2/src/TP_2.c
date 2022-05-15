7aa9aa73af1991a306a1f207a63f0ed769589713
Alcides Algarin <algarinalcides@gmail.com>
2022-05-14 22:39:49 -0300

    TP_2

---
+++ TP_2/src/TP_2.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : TP_2.c
+ Author      : Alcides Algarin  1-E
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!Hola.\nPerdon por esto, no legue a preparar el TP,\nIre trabajando en el proyecto y lo entregare cuando habiliten los recuperatorios.\nMuchas gracias,	Saludos!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
